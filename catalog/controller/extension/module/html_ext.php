d0daf01c41178edf8ce0cb783823d0aa2d8f8649
Baranov Denis <baranovda.post@gmail.com>
2025-07-09 19:48:52 +0700

    first commit

---
+++ catalog/controller/extension/module/html_ext.php
@@ -0,0 +1,26 @@
+<?php
+class ControllerExtensionModuleHTMLExt extends Controller {
+	public function index($setting) {
+		if (isset($setting['module_description'][$this->config->get('config_language_id')])) {
+			$data['heading_title'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['title'], ENT_QUOTES, 'UTF-8');
+			$code = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['description'], ENT_QUOTES, 'UTF-8');
+	        
+	        $code = preg_replace('/^\s*<\?php/', '', $code);
+            $code = preg_replace('/\?>\s*$/', '', $code);
+            $output = "";
+            ob_start();
+            try {
+                eval($code);
+                $output = ob_get_clean();
+            } catch (ParseError $e) {
+                ob_end_clean();
+                return "fuck error: " . $e->getMessage();
+            }
+
+            $data['code'] = $output;
+            
+			return $this->load->view('extension/module/html_ext', $data);
+		}
+	}
+	
+}
